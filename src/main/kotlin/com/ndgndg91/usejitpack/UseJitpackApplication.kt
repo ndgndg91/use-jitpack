4c2576ae23f13387350398cb188ff15f71c4439f
ndgndg91 <ndgndg91@gamil.com>
2024-01-14 17:22:35 +0900

    <chore> init project

---
+++ src/main/kotlin/com/ndgndg91/usejitpack/UseJitpackApplication.kt
@@ -0,0 +1,11 @@
+package com.ndgndg91.usejitpack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UseJitpackApplication
+
+fun main(args: Array<String>) {
+    runApplication<UseJitpackApplication>(*args)
+}
---
+++ src/test/kotlin/com/ndgndg91/usejitpack/UseJitpackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ndgndg91.usejitpack
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UseJitpackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
